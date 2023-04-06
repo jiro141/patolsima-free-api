8811a09a6d90074e392a424420964f849bae9e83
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-04-06 12:10:54 -0400

    Changing FaseMuestraSerializer base class

--- patolsima_api/apps/core/serializers/fase_muestra.py
+++ patolsima_api/apps/core/serializers/fase_muestra.py
@@ -1,9 +1,9 @@
-from rest_framework.serializers import Serializer
+from rest_framework.serializers import ModelSerializer
 from patolsima_api.apps.core.models import FaseMuestra
 from patolsima_api.apps.s3_management.serializers import S3FileSerializer
 
 
-class FaseMuestraSerializer(Serializer):
+class FaseMuestraSerializer(ModelSerializer):
     adjuntos = S3FileSerializer(many=True, read_only=True)
 
     class Meta:
