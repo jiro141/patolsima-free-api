907ad2b4a9233cccff3d1000631753e30144f48b
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-04-20 14:26:58 -0400

    Adding models for facturacion app

--- patolsima_api/apps/facturacion/models.py
+++ patolsima_api/apps/facturacion/models.py
@@ -1,16 +1,78 @@
 from django.db import models
+from decimal import Decimal
 from simple_history.models import HistoricalRecords
-from patolsima_api.utils.models import AuditableMixin
+from patolsima_api.utils.models import AuditableMixin, TelefonoMixin, DireccionMixin
+from patolsima_api.apps.core.models import Estudio
+from patolsima_api.apps.s3_management.models import S3File
 
 
-class ItemRecibo(AuditableMixin):
-    descripcion = models.CharField(max_length=255)
-    tipo_estudio = models.CharField(max_length=32, null=True, blank=False)
-    cantidad = models.PositiveIntegerField()
-
-
-# Create your models here.
-class AbstractRecibo(AuditableMixin):
+class Cliente(AuditableMixin, TelefonoMixin, DireccionMixin):
     razon_social = models.CharField(max_length=255)
     ci_rif = models.CharField(max_length=32)
-    telefono = models.CharField(max_length=32, null=True, blank=True)
+
+    history = HistoricalRecords()
+
+
+class Orden(AuditableMixin):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    confirmada = models.BooleanField(default=False)
+    history = HistoricalRecords()
+
+
+class ItemOrden(AuditableMixin):
+    estudio = models.OneToOneField(
+        Estudio, on_delete=models.CASCADE, related_name="item_orden"
+    )
+    orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+    monto_usd = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal("0.00")
+    )
+    history = HistoricalRecords()
+
+
+class Pago(AuditableMixin):
+    orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+
+    # Esta configuracion permite maximo montos de 99M USD
+    monto_usd = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal("0.00")
+    )
+    total_usd = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal("0.00")
+    )
+    pendiente_por_pagar_usd = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal("0.00")
+    )
+
+    # Esta configuracion permite maximo montos de 999B  Bs
+    monto_bs = models.DecimalField(
+        max_digits=14, decimal_places=2, default=Decimal("0.00")
+    )
+    total_bs = models.DecimalField(
+        max_digits=14, decimal_places=2, default=Decimal("0.00")
+    )
+    pendiente_por_pagar_bs = models.DecimalField(
+        max_digits=14, decimal_places=2, default=Decimal("0.00")
+    )
+
+    detalle = models.TextField(max_length=2048, null=True, blank=True)
+    history = HistoricalRecords()
+
+
+class NotaPago(AuditableMixin):
+    pago = models.OneToOneField(Pago, on_delete=models.CASCADE)
+    s3_file = models.OneToOneField(S3File, on_delete=models.CASCADE)
+    history = HistoricalRecords()
+
+
+class Recibo(AuditableMixin):
+    orden = models.OneToOneField(Orden, on_delete=models.CASCADE)
+    s3_file = models.OneToOneField(S3File, on_delete=models.CASCADE)
+    fecha_generacion = models.DateTimeField(null=True, blank=True)
+    history = HistoricalRecords()
+
+
+class Factura(Recibo):
+    n_factura = models.PositiveIntegerField(unique=True, db_index=True)
+
+    history = HistoricalRecords()
