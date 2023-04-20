1619d3224c9d6b753cac5881ebb208aa3af60c1a
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-04-20 14:15:04 -0400

    Changing CORE models a little bit

--- patolsima_api/apps/core/models/patologo.py
+++ patolsima_api/apps/core/models/patologo.py
@@ -1,12 +1,11 @@
 from django.db import models
 from datetime import date
 from simple_history.models import HistoricalRecords
-from patolsima_api.utils.models import NombreMixin, AuditableMixin
+from patolsima_api.utils.models import NombreMixin, AuditableMixin, NCOMEDMixin
 from patolsima_api.apps.s3_management.models import S3File
 
 
-class Patologo(AuditableMixin, NombreMixin):
-    ncomed = models.CharField(max_length=32, unique=True, null=True, blank=True)
+class Patologo(AuditableMixin, NombreMixin, NCOMEDMixin):
     firma = models.ForeignKey(S3File, on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self):
--- patolsima_api/utils/models/personal_info.py
+++ patolsima_api/utils/models/personal_info.py
@@ -9,11 +9,41 @@ class NombreMixin(models.Model):
         abstract = True
 
 
-class PersonalInfoMixin(NombreMixin):
-    direccion = models.CharField(max_length=255, null=True, blank=True)
+class EmailMixin(models.Model):
     email = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class DireccionMixin(models.Model):
+    direccion = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class TelefonoMixin(models.Model):
     telefono_fijo = models.CharField(max_length=255, null=True, blank=True)
     telefono_celular = models.CharField(max_length=255, null=True, blank=True)
 
+    class Meta:
+        abstract = True
+
+
+class NCOMEDMixin(models.Model):
+    """
+    Holds the NCOMED (numero/codigo del colegio de medicos de Venezuela
+    """
+
+    ncomed = models.CharField(
+        max_length=32, unique=True, db_index=True, null=True, blank=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class PersonalInfoMixin(NombreMixin, TelefonoMixin, DireccionMixin, EmailMixin):
     class Meta:
         abstract = True
