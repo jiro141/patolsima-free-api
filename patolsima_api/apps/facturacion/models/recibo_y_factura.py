327a8abfd056f14139b210f0bd85a348f0fedbe5
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-04-21 19:26:51 -0400

    Fixing first definition of Factura and Recibo models. Now Factura is an independent model

--- patolsima_api/apps/facturacion/models/recibo_y_factura.py
+++ patolsima_api/apps/facturacion/models/recibo_y_factura.py
@@ -6,14 +6,19 @@ from patolsima_api.apps.s3_management.models import S3File
 from .orden import Orden
 
 
-class Recibo(AuditableMixin):
+class AbstractRecibo(AuditableMixin):
     orden = models.OneToOneField(Orden, on_delete=models.CASCADE)
     s3_file = models.OneToOneField(S3File, on_delete=models.CASCADE)
-    fecha_generacion = models.DateTimeField(null=True, blank=True)
+    fecha_generacion = models.DateTimeField(null=True, blank=True, db_index=True)
+
+    class Meta:
+        abstract = True
+
+
+class Factura(AbstractRecibo):
+    n_factura = models.PositiveIntegerField(unique=True, db_index=True)
     history = HistoricalRecords()
 
 
-class Factura(Recibo):
-    n_factura = models.PositiveIntegerField(unique=True, db_index=True)
-
+class Recibo(AbstractRecibo):
     history = HistoricalRecords()
