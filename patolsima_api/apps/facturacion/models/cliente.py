9ee5881a58f03129ee4d42ab2178f2cccb0c4f8b
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-04-21 19:05:32 -0400

    Adding email to Cliente

--- patolsima_api/apps/facturacion/models/cliente.py
+++ patolsima_api/apps/facturacion/models/cliente.py
@@ -1,9 +1,14 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
-from patolsima_api.utils.models import AuditableMixin, TelefonoMixin, DireccionMixin
+from patolsima_api.utils.models import (
+    AuditableMixin,
+    TelefonoMixin,
+    DireccionMixin,
+    EmailMixin,
+)
 
 
-class Cliente(AuditableMixin, TelefonoMixin, DireccionMixin):
+class Cliente(AuditableMixin, TelefonoMixin, DireccionMixin, EmailMixin):
     razon_social = models.CharField(max_length=255)
     ci_rif = models.CharField(max_length=32, unique=True, db_index=True)
 
