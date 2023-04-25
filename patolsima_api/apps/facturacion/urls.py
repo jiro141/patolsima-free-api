99f3ebbfd6eb6455875fd20b980998536248019e
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-04-24 22:03:48 -0400

    Adding Pago to the routes of Facturacion app

--- patolsima_api/apps/facturacion/urls.py
+++ patolsima_api/apps/facturacion/urls.py
@@ -3,11 +3,13 @@ from rest_framework import routers
 from patolsima_api.apps.facturacion.views import (
     ClienteViewSet,
     OrdenViewSet,
+    PagoViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register(r"clientes", ClienteViewSet, basename="cliente")
 router.register(r"ordenes", OrdenViewSet, basename="orden")
+router.register(r"pagos", PagoViewSet, basename="pago")
 
 urlpatterns = router.urls
 
