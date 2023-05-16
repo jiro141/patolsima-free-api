3adfc871a2d9c516061f6493191d9445404e5847
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-05-15 22:16:57 -0400

    Adding ItemOrden to the list of views (missing before)

--- patolsima_api/apps/facturacion/urls.py
+++ patolsima_api/apps/facturacion/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from patolsima_api.apps.facturacion.views import (
     ClienteViewSet,
     OrdenViewSet,
+    ItemOrdenViewSet,
     PagoViewSet,
     CambioDelDiaView,
 )
@@ -11,6 +12,7 @@ from patolsima_api.apps.facturacion.views import (
 router = routers.DefaultRouter()
 router.register(r"clientes", ClienteViewSet, basename="cliente")
 router.register(r"ordenes", OrdenViewSet, basename="orden")
+router.register(r"itemsorden", ItemOrdenViewSet, basename="item-orden")
 router.register(r"pagos", PagoViewSet, basename="pago")
 urlpatterns = [path(r"cambiodeldia", CambioDelDiaView.as_view()), *router.urls]
 
