d981de90408cbd06bb73e464535b7e59480b0495
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-05-25 19:27:41 -0400

    Making S3 to work

--- patolsima_api/apps/uploaded_file_management/utils/storage_adapters/__init__.py
+++ patolsima_api/apps/uploaded_file_management/utils/storage_adapters/__init__.py
@@ -8,7 +8,6 @@ def get_default_storage_adapter():
     if not (settings.AWS_ACCESS_KEY and settings.AWS_SECRET_KEY):
         return LocalFileSystemStorageAdapter()
 
-    print("using S3")
     return S3StorageAdapter()
 
 
@@ -16,7 +15,7 @@ def get_uri_from_storage_adapter(file: UploadedFile):
     if file.storage_unit == UploadedFile.StorageUnit.LOCAL_STORAGE:
         return LocalFileSystemStorageAdapter.get_uri_for_file(file)
 
-    raise NotImplementedError()
+    return S3StorageAdapter.get_uri_for_file(file)
 
 
 __all__ = [get_default_storage_adapter, get_uri_from_storage_adapter]
--- patolsima_api/utils/singleton.py
+++ patolsima_api/utils/singleton.py
@@ -6,7 +6,6 @@ class Singleton(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        print("calling singleton")
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
