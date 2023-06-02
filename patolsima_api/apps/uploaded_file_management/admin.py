02fb455c3e8320657464845b5d92eaa871d176b4
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-06-02 16:40:07 -0400

    Adding admin view for managing UploadedFiles

--- patolsima_api/apps/uploaded_file_management/admin.py
+++ patolsima_api/apps/uploaded_file_management/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from patolsima_api.utils.admin import date_to_admin_readable
+from patolsima_api.apps.uploaded_file_management.models import UploadedFile
 
-# Register your models here.
+
+class UploadedFileAdmin(SimpleHistoryAdmin):
+    list_display = (
+        "uuid",
+        "file_name",
+        "size",
+        "content_type",
+        "storage_unit",
+        "last_time_requested_hr",
+    )
+    search_fields = ("uuid", "file_name")
+    list_filter = (
+        "storage_unit",
+        "bucket_name",
+        "created_at",
+        "updated_at",
+        "content_type",
+    )
+
+    @admin.display(ordering="last_time_requested", description="Last Time Requested")
+    def last_time_requested_hr(self, obj: UploadedFile):
+        return date_to_admin_readable(obj.last_time_requested)
+
+
+admin.site.register(UploadedFile, UploadedFileAdmin)
