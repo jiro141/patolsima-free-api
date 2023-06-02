c71ff37abf080233e6c8be14fce0d74fd570a48b
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-06-02 16:40:51 -0400

    Adding User model to Patologo as a verification relationship

--- patolsima_api/apps/core/models/patologo.py
+++ patolsima_api/apps/core/models/patologo.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import date
 from simple_history.models import HistoricalRecords
 from patolsima_api.utils.models import NombreMixin, AuditableMixin, NCOMEDMixin
@@ -7,6 +8,7 @@ from patolsima_api.apps.uploaded_file_management.models import UploadedFile
 
 class Patologo(AuditableMixin, NombreMixin, NCOMEDMixin):
     firma = models.ForeignKey(UploadedFile, on_delete=models.DO_NOTHING, null=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
 
     class Meta:
         constraints = [
@@ -14,7 +16,12 @@ class Patologo(AuditableMixin, NombreMixin, NCOMEDMixin):
                 fields=["ncomed"],
                 condition=models.Q(deleted_at=None),
                 name="patologo_ncomed_unique_when_not_deleted",
-            )
+            ),
+            models.UniqueConstraint(
+                fields=["user"],
+                condition=models.Q(deleted_at=None),
+                name="patologo_user_unique_when_not_deleted",
+            ),
         ]
 
     def __str__(self):
