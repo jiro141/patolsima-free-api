e05856636050fedd8404a15b0150bcd0c338261f
Jesús Alfredo Reyes vargas <jesusareyesv.ve@gmail.com>
2023-06-26 12:55:28 -0400

    Adding an extra function to check user's groups

--- patolsima_api/utils/users.py
+++ patolsima_api/utils/users.py
@@ -6,6 +6,10 @@ def check_user_is_in_group(user: User, group_name: str) -> bool:
     return user.groups.filter(name=group_name).exists()
 
 
-def check_user_is_in_groups(user: User, group_names: Iterable[str]) -> bool:
+def check_user_is_in_all_groups(user: User, group_names: Iterable[str]) -> bool:
     group_names = set(group_names)
     return user.groups.filter(name__in=group_names).count() == len(group_names)
+
+
+def check_user_is_in_any_group(user: User, group_names: Iterable[str]) -> bool:
+    return user.groups.filter(name__in=group_names).exists()
