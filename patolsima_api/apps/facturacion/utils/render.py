7f860864f5af05c827b104014ffc3d902f9734ab
minesto <minesto23@gmail.com>
2023-11-25 17:22:09 +0100

    factura fix

--- patolsima_api/apps/facturacion/utils/render.py
+++ patolsima_api/apps/facturacion/utils/render.py
@@ -40,7 +40,7 @@ NOTA_DE_PAGO_TEMPLATES = {
 }
 
 
-def render_recibo_factura(registro: Union[Factura, Recibo], tipo: str, n_documento:int) -> str:
+def render_recibo_factura(registro: Union[Factura, Recibo], tipo: str) -> str:
     """
     This method takes a Factura/Recibo instance and renders its associated pdf.
     :param registro: Factura/Recibo instance. The Orden instance associated must be pagada=True.
@@ -48,8 +48,7 @@ def render_recibo_factura(registro: Union[Factura, Recibo], tipo: str, n_documen
     """
 
     factura = Factura.objects.get(orden=registro.orden)
-    numero_documento = registro.id if tipo == "recibo" else "pajudo"
-    numero_documento = "pajudo"
+    numero_documento = registro.id if tipo == "recibo" else factura.n_factura
     documento_tipo = "Orden" if tipo == "recibo" else "Factura"
     orden = numero_documento if documento_tipo == "Factura" else registro.orden
     filename = f"{tipo}_{registro.orden.id}_{int(time.time())}"
