fb35fad40da76673cf359e2355fd1b6b04d3ec05
minesto <minesto23@gmail.com>
2023-11-28 21:48:16 +0100

    factura fix

--- patolsima_api/apps/facturacion/utils/orden.py
+++ patolsima_api/apps/facturacion/utils/orden.py
@@ -78,8 +78,10 @@ def generar_recibo_o_factura(orden: Orden, tipo_documento: str, **kwargs) -> Rec
              if f_transaccion.n_control >= n_factura:
                 n_factura=f_transaccion.n_documento + 1
 
-
-        instancia_de_documento, created = (Factura).objects.get_or_create(orden=orden,n_factura=n_factura, defaults=kwargs)
+        if NotasCredito.objects.get(orden=orden):
+            instancia_de_documento, created = (Factura).objects.create(orden=orden,n_factura=n_factura,defaults=kwargs)
+        else:
+            instancia_de_documento, created = (Factura).objects.get_or_create(orden=orden,n_factura=n_factura, defaults=kwargs)
         instancia_de_documento.monto = orden.importe_orden_bs
         n_controlobject = Transaccion.objects.last()
         n_control = n_controlobject.n_control + 1
